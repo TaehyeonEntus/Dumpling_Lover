1f02d2fe06b570197c13e08beeced44bfc529000
KTH <taehyeonentus@gmail.com>
2024-10-26 18:07:27 +0900

    SpringBoot 추가

---
+++ dumpling/dumpling/src/main/java/com/list/dumpling/DumplingApplication.java
@@ -0,0 +1,13 @@
+package com.list.dumpling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumplingApplication.class, args);
+	}
+
+}
---
+++ dumpling/dumpling/src/test/java/com/list/dumpling/DumplingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.dumpling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumplingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
